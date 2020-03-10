8e58ea40f539ddf933c3f340cecf8db17d6a7772
pm2111 <peter_dimmar@yahoo.co.uk>
2020-03-10 14:22:15 +0000

    2 scars RV along intersheet direction, run 1

--- scar_DTI003_fiber.m
+++ scar_DTI003_fiber.m
@@ -61,10 +61,179 @@ RV_posterior=0;
                         %  width=12;
                           length=10;
                         length=length/10;
-                        width=1.1*width/2;
+                        width=width/2;
                         
                        %  scar_properties ={};
             if exist('cursor_info')==0
+                x = -0.989;
+                 y = -9.9524;
+                 z = -9.4597;
+            else
+                x= cursor_info.Position(1); %RV post [x,y,z]=[-0.989, -9.9524,-9.4597]
+                     y=    cursor_info.Position(2);
+                     z= cursor_info.Position(3);
+            end
+            
+             ts_scaling_no_fib=ones(23,1);
+
+               epi1= knnsearch(H_mesh.xyz(  H_mesh.epi,:),[x,y,z]);
+         endo1=knnsearch(H_mesh.xyz(H_mesh.rv,:),H_mesh.xyz(epi1,:));
+         
+         mid1= (H_mesh.xyz(H_mesh.epi(epi1),:)+H_mesh.xyz(H_mesh.rv(endo1),:))/2;
+         
+         mid1_id= knnsearch(H_mesh.xyz,mid1);
+         
+         points = [mid1_id];
+         if RV_posterior==1
+             for i=1:50
+                 [rw,col]=find(H_mesh.tri==mid1_id);
+                  fiber_dir = -H_mesh.triORTHOfull(col(1),1:3);%use the direction in the fiber dir
+                % fiber_dir = -fibers_normal(col(1),1:3);
+                 mid1 = mid1+fiber_dir*(length/3);
+                  id_next=knnsearch(H_mesh.xyz,mid1);
+                  points = [points,id_next];
+                 dists= distance(H_mesh.xyz,mid1);
+                 keep=find(dists<width);
+                points = horzcat(points,keep');
+                points=unique(points);
+
+                  mid1_id=id_next;
+                  mid1= H_mesh.xyz(id_next,:);
+             end
+         
+         elseif RV_mid ==1
+              for i=1:35
+                 [rw,col]=find(H_mesh.tri==mid1_id);
+                  fiber_dir = -H_mesh.triORTHOfull(col(1),1:3);%use the direction in the fiber dir
+                % fiber_dir = -fibers_normal(col(1),1:3);
+                 mid1 = mid1+fiber_dir*(length/3);
+                  id_next=knnsearch(H_mesh.xyz,mid1);
+                  points = [points,id_next];
+                 dists= distance(H_mesh.xyz,mid1);
+                 keep=find(dists<width);
+                points = horzcat(points,keep');
+                points=unique(points);
+
+                  mid1_id=id_next;
+                  mid1= H_mesh.xyz(id_next,:);
+              end
+         else
+              for i=1:50
+                 [rw,col]=find(H_mesh.tri==mid1_id);
+                  fiber_dir = -H_mesh.triORTHOfull(col(1),1:3);%use the direction in the fiber dir
+                % fiber_dir = -fibers_normal(col(1),1:3);
+                 mid1 = mid1+fiber_dir*(length/3);
+                  id_next=knnsearch(H_mesh.xyz,mid1);
+                  points = [points,id_next];
+                 dists= distance(H_mesh.xyz,mid1);
+                 keep=find(dists<2);
+                points = horzcat(points,keep');
+                points=unique(points);
+
+                  mid1_id=id_next;
+                  mid1= H_mesh.xyz(id_next,:);
+              end
+             
+         end
+         
+         
+         %find the points closest yo you in the normal to fiber dir both
+         %dirs) and add them to the list
+                    no_lge= ones(size(H_mesh.xyz,1),1);
+
+                     no_lge(points)=density;
+% 
+% 
+%                  figure()
+%                  headlight
+%                  hold on
+%                  patch('vertices',H_mesh.xyz,'faces',H_mesh.face,'edgecolor','none','FaceVertexCData',no_lge,'facecolor','interp')
+%                  colorbar
+%                  axis off
+%                  view([-115 -56])
+         for i=1:width%:5*width
+             %take step in direction
+             new_coords_up = H_mesh.xyz(points,:) + i*fibers_normal(points,:)/50.0;
+             new_coords_down = H_mesh.xyz(points,:) - i*fibers_normal(points,:)/50.0;
+             new_ids_up=knnsearch(H_mesh.xyz,new_coords_up);        
+             new_ids_down=knnsearch(H_mesh.xyz,new_coords_down);  
+             points =[points,new_ids_up',new_ids_down'];
+             new_coords_up = [];
+                new_coords_down = [];
+                new_ids_up =[];
+                new_ids_down=[];
+         end
+         
+        
+%   
+%            
+           no_lge= ones(size(H_mesh.xyz,1),1);
+           
+%            dist_to_point = distance(mid1,H_mesh.xyz(border_ids,:));
+           % dist_to_point_norm= dist_to_point/max(dist_to_point);
+            
+            
+            no_lge(points)=density;
+            % no_lge(points)=i*dist_to_point_norm;
+            dummy=find(no_lge)<1;
+            no_lge(dummy)=1;
+            
+      
+            
+         
+%             
+ figure()
+ headlight
+ hold on
+ patch('vertices',H_mesh.xyz,'faces',H_mesh.face,'edgecolor','none','FaceVertexCData',no_lge,'facecolor','interp')
+ colorbar
+ axis off
+ view([-115 -56])
+%             
+%            % light_lge(border_ids)=20*i;
+% 
+%              int_lge(scar_ids)=4*max(int_lge);
+%             adv_lge(scar_ids)=4*max(adv_lge);
+
+%            no_lge(H_mesh.rv)=1;
+%            light_lge(H_mesh.rv)=1;
+%            int_lge(H_mesh.rv)=1;
+%            adv_lge(H_mesh.rv)=1;
+%            
+%            no_lge(H_mesh.lv)=1;
+%            light_lge(H_mesh.lv)=1;
+%            int_lge(H_mesh.lv)=1;
+%            adv_lge(H_mesh.lv)=1;
+        
+           
+           % dlmwrite(strcat(patient_sim_folder,'scalings\eikonal06_coarse_fib_',num2str(0),'.csv'), no_lge,'precision',10);
+           dlmwrite(strcat(patient_sim_folder,'scars_RV_anterior\scar_CV_slowing_factor',num2str(density),'_initial_length_',num2str(length),'_growth_factor_',num2str(width),'.csv'), no_lge,'precision',10);
+         %   dlmwrite(strcat(patient_sim_folder,'scalings\eikonal06_coarse_fib_',num2str(2),'_scar.csv'),  int_lge,'precision',10);
+          %  dlmwrite(strcat(patient_sim_folder,'scalings\eikonal06_coarse_fib_',num2str(3),'_scar.csv'),  adv_lge,'precision',10);
+           end
+            end
+        end
+      %  end
+       % end
+        
+       
+       
+RV_mid=0;
+RV_posterior=1;
+RV_anterior=0;
+        for width =[1,2,3]
+        for j=6
+            for density=5
+                %for t=1:8
+                  %  for r=1:8
+                          k=0;
+                        %  width=12;
+                          length=10;
+                        length=length/10;
+                        width=width/2;
+                        
+                       %  scar_properties ={};
+            if RV_posterior==1
                 x = -0.989;
                  y = -9.9524;
                  z = -9.4597;
@@ -216,238 +385,7 @@ RV_posterior=0;
       %  end
        % end
         
-       
-       
-       %for rv mid scar, increase stepping size ? the scars are smaller.. 
-        for width =[1,2,3]
-        for j=6
-            for density=5
-                %for t=1:8
-                  %  for r=1:8
-                          k=0;
-                        %  width=12;
-                             length=10;
-                        length=length/9;
-                        width=width/2;
-                        
-                       %  scar_properties ={};
-            if exist('cursor_info')==0
-                x = -0.989;
-                 y = -9.9524;
-                 z = -9.4597;
-            else
-                x= cursor_info.Position(1); %RV post [x,y,z]=[-0.989, -9.9524,-9.4597]
-                     y=    cursor_info.Position(2);
-                     z= cursor_info.Position(3);
-            end
-            
-             ts_scaling_no_fib=ones(23,1);
-
-               epi1= knnsearch(H_mesh.xyz(  H_mesh.epi,:),[x,y,z]);
-         endo1=knnsearch(H_mesh.xyz(H_mesh.rv,:),H_mesh.xyz(epi1,:));
-         
-         mid1= (H_mesh.xyz(H_mesh.epi(epi1),:)+H_mesh.xyz(H_mesh.rv(endo1),:))/2;
-         
-         mid1_id= knnsearch(H_mesh.xyz,mid1);
-         
-         points = [mid1_id];
-         for i=1:50
-             [rw,col]=find(H_mesh.tri==mid1_id);
-              fiber_dir = -H_mesh.triORTHOfull(col(1),1:3);%use the direction in the fiber dir
-            % fiber_dir = -fibers_normal(col(1),1:3);
-             mid1 = mid1+fiber_dir*(length/3);
-              id_next=knnsearch(H_mesh.xyz,mid1);
-              points = [points,id_next];
-             dists= distance(H_mesh.xyz,mid1);
-             keep=find(dists<width);
-            points = horzcat(points,keep');
-            points=unique(points);
-
-              mid1_id=id_next;
-              mid1= H_mesh.xyz(id_next,:);
-         end
-         
-         %find the points closest yo you in the normal to fiber dir both
-         %dirs) and add them to the list
-         
-         for i=1:width%:5*width
-             %take step in direction
-             new_coords_up = H_mesh.xyz(points,:) + i*fibers_normal(points,:)/50.0;
-             new_coords_down = H_mesh.xyz(points,:) - i*fibers_normal(points,:)/50.0;
-             new_ids_up=knnsearch(H_mesh.xyz,new_coords_up);        
-             new_ids_down=knnsearch(H_mesh.xyz,new_coords_down);  
-             points =[points,new_ids_up',new_ids_down'];
-             new_coords_up = [];
-                new_coords_down = [];
-                new_ids_up =[];
-                new_ids_down=[];
-         end
-         
         
-%   
-%            
-           no_lge= ones(size(H_mesh.xyz,1),1);
-           
-%            dist_to_point = distance(mid1,H_mesh.xyz(border_ids,:));
-           % dist_to_point_norm= dist_to_point/max(dist_to_point);
-            
-            
-            no_lge(points)=density;
-            % no_lge(points)=i*dist_to_point_norm;
-            dummy=find(no_lge)<1;
-            no_lge(dummy)=1;
-            
-      
-            
-         
-%             
- figure()
- headlight
- hold on
- patch('vertices',H_mesh.xyz,'faces',H_mesh.face,'edgecolor','none','FaceVertexCData',no_lge,'facecolor','interp')
- colorbar
- axis off
- view([-115 -56])
-%             
-%            % light_lge(border_ids)=20*i;
-% 
-%              int_lge(scar_ids)=4*max(int_lge);
-%             adv_lge(scar_ids)=4*max(adv_lge);
-
-%            no_lge(H_mesh.rv)=1;
-%            light_lge(H_mesh.rv)=1;
-%            int_lge(H_mesh.rv)=1;
-%            adv_lge(H_mesh.rv)=1;
-%            
-%            no_lge(H_mesh.lv)=1;
-%            light_lge(H_mesh.lv)=1;
-%            int_lge(H_mesh.lv)=1;
-%            adv_lge(H_mesh.lv)=1;
-        
-           
-           % dlmwrite(strcat(patient_sim_folder,'scalings\eikonal06_coarse_fib_',num2str(0),'.csv'), no_lge,'precision',10);
-          % dlmwrite(strcat(patient_sim_folder,'scars_RV_inferior\scar_CV_slowing_factor',num2str(density),'_initial_length_',num2str(length),'_growth_factor_',num2str(width),'.csv'), no_lge,'precision',10);
-         %   dlmwrite(strcat(patient_sim_folder,'scalings\eikonal06_coarse_fib_',num2str(2),'_scar.csv'),  int_lge,'precision',10);
-          %  dlmwrite(strcat(patient_sim_folder,'scalings\eikonal06_coarse_fib_',num2str(3),'_scar.csv'),  adv_lge,'precision',10);
-           end
-            end
-        end
-        
-              %for rv anterior scar
-        for width =[1,2,3]
-        for j=6
-            for density=5
-                %for t=1:8
-                  %  for r=1:8
-                          k=0;
-                        %  width=12;
-                             length=10;
-                        length=length/9;
-                        width=width/2;
-                        
-                       %  scar_properties ={};
-            if exist('cursor_info')==0
-                x = -0.989;
-                 y = -9.9524;
-                 z = -9.4597;
-            else
-                x= cursor_info.Position(1); %RV post [x,y,z]=[-0.989, -9.9524,-9.4597]
-                     y=    cursor_info.Position(2);
-                     z= cursor_info.Position(3);
-            end
-            
-             ts_scaling_no_fib=ones(23,1);
-
-               epi1= knnsearch(H_mesh.xyz(  H_mesh.epi,:),[x,y,z]);
-         endo1=knnsearch(H_mesh.xyz(H_mesh.rv,:),H_mesh.xyz(epi1,:));
-         
-         mid1= (H_mesh.xyz(H_mesh.epi(epi1),:)+H_mesh.xyz(H_mesh.rv(endo1),:))/2;
-         
-         mid1_id= knnsearch(H_mesh.xyz,mid1);
-         
-         points = [mid1_id];
-         for i=1:50
-             [rw,col]=find(H_mesh.tri==mid1_id);
-              fiber_dir = -H_mesh.triORTHOfull(col(1),1:3);%use the direction in the fiber dir
-            % fiber_dir = -fibers_normal(col(1),1:3);
-             mid1 = mid1+fiber_dir*(length/3);
-              id_next=knnsearch(H_mesh.xyz,mid1);
-              points = [points,id_next];
-             dists= distance(H_mesh.xyz,mid1);
-             keep=find(dists<width);
-            points = horzcat(points,keep');
-            points=unique(points);
-
-              mid1_id=id_next;
-              mid1= H_mesh.xyz(id_next,:);
-         end
-         
-         %find the points closest yo you in the normal to fiber dir both
-         %dirs) and add them to the list
-         
-         for i=1:width%:5*width
-             %take step in direction
-             new_coords_up = H_mesh.xyz(points,:) + i*fibers_normal(points,:)/50.0;
-             new_coords_down = H_mesh.xyz(points,:) - i*fibers_normal(points,:)/50.0;
-             new_ids_up=knnsearch(H_mesh.xyz,new_coords_up);        
-             new_ids_down=knnsearch(H_mesh.xyz,new_coords_down);  
-             points =[points,new_ids_up',new_ids_down'];
-             new_coords_up = [];
-                new_coords_down = [];
-                new_ids_up =[];
-                new_ids_down=[];
-         end
-         
-        
-%   
-%            
-           no_lge= ones(size(H_mesh.xyz,1),1);
-           
-%            dist_to_point = distance(mid1,H_mesh.xyz(border_ids,:));
-           % dist_to_point_norm= dist_to_point/max(dist_to_point);
-            
-            
-            no_lge(points)=density;
-            % no_lge(points)=i*dist_to_point_norm;
-            dummy=find(no_lge)<1;
-            no_lge(dummy)=1;
-            
-      
-            
-         
-%             
- figure()
- headlight
- hold on
- patch('vertices',H_mesh.xyz,'faces',H_mesh.face,'edgecolor','none','FaceVertexCData',no_lge,'facecolor','interp')
- colorbar
- axis off
- view([-115 -56])
-%             
-%            % light_lge(border_ids)=20*i;
-% 
-%              int_lge(scar_ids)=4*max(int_lge);
-%             adv_lge(scar_ids)=4*max(adv_lge);
-
-%            no_lge(H_mesh.rv)=1;
-%            light_lge(H_mesh.rv)=1;
-%            int_lge(H_mesh.rv)=1;
-%            adv_lge(H_mesh.rv)=1;
-%            
-%            no_lge(H_mesh.lv)=1;
-%            light_lge(H_mesh.lv)=1;
-%            int_lge(H_mesh.lv)=1;
-%            adv_lge(H_mesh.lv)=1;
-        
-           
-           % dlmwrite(strcat(patient_sim_folder,'scalings\eikonal06_coarse_fib_',num2str(0),'.csv'), no_lge,'precision',10);
-          % dlmwrite(strcat(patient_sim_folder,'scars_RV_inferior\scar_CV_slowing_factor',num2str(density),'_initial_length_',num2str(length),'_growth_factor_',num2str(width),'.csv'), no_lge,'precision',10);
-         %   dlmwrite(strcat(patient_sim_folder,'scalings\eikonal06_coarse_fib_',num2str(2),'_scar.csv'),  int_lge,'precision',10);
-          %  dlmwrite(strcat(patient_sim_folder,'scalings\eikonal06_coarse_fib_',num2str(3),'_scar.csv'),  adv_lge,'precision',10);
-           end
-            end
-        end
-
  figure()
  headlight
  hold on
